e301659834f66d27f0375a5cc4fea653a5e60641
b1tflyyyy <thisisandrew0110@gmail.com>
2024-05-26 07:28:12 -0400

    added filling of external rectangle

--- source.asm
+++ source.asm
@@ -5,7 +5,10 @@ global _start
 %define SYS_WRITE 1
 %define STDOUT 1
 %define SYS_EXIT 60
+%define SYS_NANOSLEEP 35
+
 %define EXTERNAL_RECTANGLE_SYMBOL 35
+%define INTERNAL_RECTANGLE_SYMBOL 62
  
 section .rodata
 buffer_width equ 128
@@ -16,6 +19,8 @@ buffer resb buffer_height * buffer_width
 
 section .text           
 _start:
+    sub rsp, 40 ; reserve 40 bytes
+    
     call init_buffer ; set ' ' for all elements and '\n' at the end
     
     ; ======== draw_horizontal_line ========
@@ -31,7 +36,6 @@ _start:
     call draw_horizontal_line ; draw low line
 
     ; ======== draw_vertical_line ========
-    
     mov rdi, 1 ; Y = 1
     mov rsi, buffer_height - 2 ; - start & end symbol
     
@@ -49,8 +53,62 @@ _start:
     lea rsi, buffer
     mov rdx, buffer_width * buffer_height
 
-    syscall                 
+    syscall 
+
+    ; ======== animation ======== 
+    lea r10, buffer
+    add r10, 129 ; skip first line    
     
+    mov qword [rsp + 16], buffer_width * buffer_height - 256; total iterations counter, -256 does not write last line 
+    mov qword [rsp + 24], 128 ; current symbol position   
+    mov qword [rsp + 32], 253 ; last symbol position
+
+_anim_loop:
+    mov qword r9, [rsp + 32]
+    
+    cmp qword [rsp + 24], r9
+    je _last_symbol
+
+    mov byte [r10], INTERNAL_RECTANGLE_SYMBOL
+    
+    ; ======== write ========
+    mov rax, SYS_WRITE    ; number of sys function write
+    mov rdi, STDOUT       ; set standard out stream
+    
+    lea rsi, buffer
+    mov rdx, buffer_width * buffer_height
+
+    syscall
+
+    ; ========= sleep ========
+    mov qword [rsp], 0
+    mov qword [rsp + 8], 10000 * 100 * 20
+
+    lea rdi, [rsp]
+    xor rsi, rsi
+
+    mov rax, SYS_NANOSLEEP
+    syscall
+
+_inc:
+    inc r10 ; next position in buffer
+    inc qword [rsp + 24] ; set current position + 1
+    dec qword [rsp + 16] ; dec total iterations counter
+    
+    jnz _anim_loop
+    jmp _exit_anim_loop
+
+_last_symbol:
+    add r10, 2 ; skip 2 positions -> '#' + '\n'
+    
+    inc qword [rsp + 24] ; set current position + 1
+    sub qword [rsp + 16], 2 ; total iterations counter -2, 'cause skip 2 positions
+
+    add qword [rsp + 32], 127 ; set next last symbol position
+    jmp _inc
+
+_exit_anim_loop:
+
     ; ======== return 0 ========
     mov rax, SYS_EXIT ; number of sys function exit
     xor rdi, rdi ; 0
